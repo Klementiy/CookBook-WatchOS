437c28df451d8c327be631754c8297a760464376
Klementiy <74532062+Klementiy@users.noreply.github.com>
2020-12-10 00:07:50 +0300

    Ver.1

---
+++ CookBook WatchKit Extension/RowTable.swift
@@ -0,0 +1,15 @@
+//
+//  RowTable.swift
+//  CookBook
+//
+//  Created by user on 05/12/2020.
+//
+
+import WatchKit
+
+class RowTable: NSObject {
+
+    @IBOutlet weak var rowPicture: WKInterfaceImage!
+    @IBOutlet weak var rowRecipeName: WKInterfaceLabel!
+    
+}
